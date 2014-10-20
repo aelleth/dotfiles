059d4071a741901fa8be249c42c83b563f6bd926
Peter Tsonchev <peter@innervate.me>
2014-10-20 13:15:50 +0000

    install.sh: add list of required binaries
    If any of the binaries are not present (not installed or missing from
    PATH), report their absence and exit with an error.

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 script_dir=$(cd $(dirname "$0") && pwd)
 
+binlist="cmake git vim"
+
+for binary in $binlist; do
+    if ! which $binary >/dev/null; then
+        >&2 echo "error: $binary is not installed"
+        exit 1
+    fi
+done
+
 if [ ! -e ~/.zshrc ]; then
     ln -s $script_dir/zsh/zshrc ~/.zshrc
 fi
